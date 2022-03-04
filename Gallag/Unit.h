2ab602e959c60d9b2bfd2b1d801bbb6668c8e78b
234234 <sdsdfsf>
2022-03-04 16:47:41 +0900

    Class : screen -> Class : StartScreen 이름 변경, cols=150,lines=50 변경

--- Gallag/Unit.h
+++ Gallag/Unit.h
@@ -10,3 +10,8 @@ class Ally
 
 
 };
+
+class Enemy
+{
+
+};
--- Gallag/startcreen.cpp
+++ Gallag/startcreen.cpp
@@ -1,11 +1,11 @@
 #include "Main.h"
 
-class Screen {
+class StartScreen {
 
 private:
 	int startsc()
 	{
-		system("mode con: cols=100 lines=50");
+		system("mode con: cols=150 lines=50");
 		_getch();
 	}
 public:
