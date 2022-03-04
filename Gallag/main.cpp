039bec023d90068c940aad379353963145684129
Andrew Han <andrew@Andrew-Notebook.local>
2022-03-04 17:39:09 +0900

    갤러그표지

--- Gallag/main.cpp
+++ Gallag/main.cpp
@@ -1,7 +1,11 @@
-#include <iostream>
+#include <stdio.h>
 
-int main(void) {
-
-
+int main() { 
+	printf("\n\n\n\n"); // 맨 위 에 4칸 개 행
+	printf("   #       #       #      #         #####        #        #####       #####  \n");
+	printf("    #     # #     #      # #        #    #      # #         #         #    # \n");
+	printf("     #   #   #   #      # # #       #####      # # #        #         #####  \n");
+	printf("      # #     # #      #     #      #  #      #     #       #         #  #   \n");
+	printf("       #       #      #       #     #   #    #       #    #####       #   #  \n");
 	return 0;
 }
