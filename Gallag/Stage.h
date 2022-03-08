a1ce726c83034f7fc47a6ee7219e4b0a7cef7acb
Noelllll1223 <neo9897@gmail.com>
2022-03-08 17:53:54 +0900

    0308update

--- Gallag/Stage.h
+++ Gallag/Stage.h
@@ -27,3 +27,38 @@ public:
 	}
 
 };
+
+class Guide : Stage
+{
+private:
+	int x, y;
+public:
+	Guide(int ax, int ay) { x = ax; y = ay; }
+	void GuideControl() {
+		gotoxy(x, y); printf("<x> : 적군은 이렇습니다 1포인트를 줍니다.");
+		gotoxy(x, y+1); printf("<o> : 아군은 이렇습니다.");
+		gotoxy(x, y + 2); printf("<H> : 하트를 채워줍니다.");
+		gotoxy(x, y + 3); printf("<B> : 화면에 있는 적을 없애버립니다.");
+		gotoxy(x, y + 4); printf("<P> : 공격력을 상승시킵니다.");
+	}
+
+};
+
+class inGame : Stage
+{
+private:
+	int x, y;
+public:
+	inGame(int ax, int ay) { x = ax; y = ay; }
+	void life() {
+		gotoxy(x, y); printf("Life : ", ); //하트 asc코드 확인하고 해당 부분 아군기체 피해시 깍는 로직 필요
+
+	}
+	void score() {
+		gotoxy(x, y); printf("score: %d", ); // 점수 쌓는 로직 생성 후 추가 필요
+	}
+	void ReturnScreen() {
+		gotoxy(x, y); printf("R: 이전화면");
+
+	}
+};
