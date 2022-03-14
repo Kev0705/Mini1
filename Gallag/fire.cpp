169c86a98f4c90edb5c865bda75eb7b086dc1248
Noelllll1223 <neo9897@gmail.com>
2022-03-14 17:22:26 +0900

    firev0.3

--- Gallag/fire.cpp
+++ Gallag/fire.cpp
@@ -6,16 +6,20 @@ void Fire::FireShoot(int& x, int& y) // 내가 쏘는 발사체
         c = _getch();
         switch (c) {
         case SPACEBAR:
-            int HY = y;
+            int HY = y - 1;
             while (HY>6) {
                 
                 Sleep(20);
                 HY--;
                 gotoxy(x, HY);
                 printf("@");
+                Sleep(30);
+                gotoxy(x, HY);
+                printf(" ");
                 
             }
         }
     }
+    
 }
 
